2011b146df5fb7e0a596ef3a3fc358b79a8886ee
Robo-Sapien <shivambhagat314@gmail.com>
2019-04-01 23:45:14 +0530

    class added

---
+++ jarvis.h
@@ -0,0 +1,29 @@
+#ifndef JARVIS
+#define JARVIS
+
+#include <iostream>
+#include <vector> 
+#include <cmath>
+#include "convex_hull_utility.h"
+//#include "mainwindow.h"
+//#include <QApplication>
+
+class Jarvis: public ConvexHull 
+{
+
+private:
+	int indexOfLeftMostPoint(vector<point> &points);
+	double findAngle(point a, point b, point c);
+	int secondPointIndex(vector<point> &points);
+	int findOrientation(point a, point b, point c);
+
+public:
+	Jarvis(vector<struct point> points):ConvexHull(points)
+	{
+
+	}
+	void runJarvisMarch(vector<point> &points);
+};
+
+
+#endif 
