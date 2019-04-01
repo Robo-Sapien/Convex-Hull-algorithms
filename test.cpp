98256ed3bd027b5f4748c5858d9b510fd7a6ecf2
Abhinav <abhinavkumar.sieger@gmail.com>
2019-04-01 23:35:41 +0530

    Testing on 5 lakh point done. Takes around 30 sec.

--- test.cpp
+++ test.cpp
@@ -6,13 +6,13 @@
 
 int main(){
     //Initializing the points
-    srand(14);
+    srand(4);
     vector<struct point> points;
     vector<int> points_idx;
-    int range=500000;
+    int range=100000;
     for(int i=0;i<range;i++){
-        float x=rand()%range;
-        float y=rand()%range;
+        float x=((float)(rand()%range))/7.0;
+        float y=((float)(rand()%range))/7.0;
         int idx=i;
         //cin>>x>>y;
         struct point p;
