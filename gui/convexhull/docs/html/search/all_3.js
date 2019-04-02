240e8a2781249e8d49a6df662e2b4e86e8a09566
yashdeep97 <yashdeep97@gmail.com>
2019-04-03 00:58:03 +0530

    minor changes

--- gui/convexhull/docs/html/search/all_3.js
+++ gui/convexhull/docs/html/search/all_3.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['fill_5fslope_5fbucket',['fill_slope_bucket',['../classKirkpatrick__Seidel.html#afa21506d3cd0d121b337145af844191a',1,'Kirkpatrick_Seidel']]],
   ['findorientation',['findOrientation',['../classGrahamScan.html#a228b6d9784f514280c8a84d3a7fffa11',1,'GrahamScan']]],
   ['findreferencepointandswap',['findReferencePointAndSwap',['../classGrahamScan.html#a3d6c2b8a83af6e55436bf260e7ea8a98',1,'GrahamScan']]]
 ];
